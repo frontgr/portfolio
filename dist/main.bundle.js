4ec2d8c7e9c6ffd06a8a8d10f2e663a71d1548d3
halareka <149045655+halareka@users.noreply.github.com>
2024-06-05 01:42:33 +0700

    feat: Added dist folder

---
+++ dist/main.bundle.js
@@ -0,0 +1,34 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/scripts/module.js
+
+function draw(color){
+    console.log(`%c
+        ███████╗██████╗  ██████╗ ███╗   ██╗████████╗ ██████╗ ██████╗ 
+        ██╔════╝██╔══██╗██╔═══██╗████╗  ██║╚══██╔══╝██╔════╝ ██╔══██╗
+        █████╗  ██████╔╝██║   ██║██╔██╗ ██║   ██║   ██║  ███╗██████╔╝
+        ██╔══╝  ██╔══██╗██║   ██║██║╚██╗██║   ██║   ██║   ██║██╔══██╗
+        ██║     ██║  ██║╚██████╔╝██║ ╚████║   ██║   ╚██████╔╝██║  ██║
+        ╚═╝     ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
+    ` , `background: transparent; color: ${color}`)
+    console.log(`Drawing with color: ${color}`);
+}
+
+
+;// CONCATENATED MODULE: ./src/scripts/index.js
+
+
+
+
+const randomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
+function start(){
+    setInterval(() => {
+        console.clear();
+        draw(randomColor())
+    }, 1000);
+}
+start();
+/******/ })()
+;
