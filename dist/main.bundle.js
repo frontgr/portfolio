1e95d78835b983cf877348315bea4ea01a5d832b
nibezo <nibezo.cs@gmail.com>
2024-06-04 23:23:47 +0400

    build: Add dev mode

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -22,13 +22,16 @@ function draw(color){
 
 
 
-const randomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
-function start(){
-    setInterval(() => {
-        console.clear();
-        draw(randomColor())
-    }, 1000);
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function start() {
+  setInterval(() => {
+    console.clear();
+    draw(randomColor());
+    console.log("Wish  you great daytime!");
+  }, 1000);
 }
 start();
+
 /******/ })()
 ;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,14 @@
 import "../css/main.css";
 
-import draw from './module.js'
+import draw from "./module.js";
 
-const randomColor = () => `#${Math.floor(Math.random()*16777215).toString(16)}`;
-function start(){
-    setInterval(() => {
-        console.clear();
-        draw(randomColor())
-    }, 1000);
+const randomColor = () =>
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+function start() {
+  setInterval(() => {
+    console.clear();
+    draw(randomColor());
+    console.log("Wish  you great daytime!");
+  }, 1000);
 }
 start();
