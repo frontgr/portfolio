34d255451fad078a2977672627b08284282f0eee
halareka <149045655+halareka@users.noreply.github.com>
2024-06-19 00:59:49 +0700

    fix: Limited validate on input

--- front/src/scripts/index.js
+++ front/src/scripts/index.js
@@ -3,12 +3,18 @@ import {validate , oninputForm} from "./formValidate";
 import { jsonBuild } from "./priceBuild";
 import { select, selectTypeOfSite } from "./selectDropdown";
 
-document.getElementById("submitForm").addEventListener("click", function () {
+document.getElementById("formContacts").addEventListener("submit", function(event) {
+    event.preventDefault();
     if (validate()) {
         console.log(jsonBuild());
+        document.getElementById('formContacts').reset()
     }
 });
-
+document.getElementById("amountPage").addEventListener('input', function() {
+    if(this.value > 256){
+        document.getElementById("amountPage").value = 256;
+    }
+});
 select();
 selectTypeOfSite();
 
