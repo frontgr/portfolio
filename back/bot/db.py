125b7f47736697a6edcf3d0b5ed3ab78001d27dd
reydotcom <rey.dot.conn@gmail.com>
2024-06-21 18:34:59 +0200

    feat: A class with database operations has been created

--- back/bot/db.py
+++ back/bot/db.py
@@ -29,4 +29,32 @@ class Receivers(Document):
 
 
 
+class DB:
+    def __init__(self, admin: list):
+        self.admin = admin
+        
+        try:
+            AdminsList(tg_id=admin).save()
+        except NotUniqueError:
+            pass
+
+    @staticmethod
+    def get_admin():
+        return AdminsList.objects.first().tg_id
+
+    @staticmethod
+    def insert_request(tg_id: int, name: str, username: str): 
+        record = RequestsList(tg_id=tg_id, name=name, username=username)
+        record.save() 
+            
+    @staticmethod
+    def insert_receivers(tg_id: int, name: str, username: str):
+        record = Receivers(tg_id=tg_id, name=name, username=username)
+        record.save() 
+    
+    @staticmethod
+    def delete_request(tg_id: int):
+        record = RequestsList.objects(tg_id=tg_id).delete()
+
+
     
