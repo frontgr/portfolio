261b555adce4ad560164134efbb58d91aca69b1b
halareka <149045655+halareka@users.noreply.github.com>
2024-06-26 16:30:56 +0700

    fix: Script disabled because it is not active

--- front/src/scripts/popup-menu.js
+++ front/src/scripts/popup-menu.js
@@ -1,28 +1,59 @@
-var menu_navigation = $("#popup--navigation");
+var menu_navigation = document.querySelector("#popup--navigation");
 
-$("#popup--button").on("click", function (e) { 
+document.querySelector("#popup--button").on("click", function (e) { 
     if (!menu_navigation.is(":animated") && !menu_navigation.is(":visible")) {
         menu_navigation.fadeIn(500);
     }
 });
 
-$("#popup--button--close").on("click", function (e) { 
+document.querySelector("#popup--button--close").on("click", function (e) { 
     if (!menu_navigation.is(":animated") && menu_navigation.is(":visible")) {
         menu_navigation.fadeOut(500, function() {
         });
     }
 });
 
-$(document).on("click", ".popup-anchor-link", function (e) {
+document.querySelector(document).on("click", ".popup-anchor-link", function (e) {
     e.preventDefault();
     
-    var targetAnchor = $(this).attr("href");
+    var targetAnchor = document.querySelector(this).attr("href");
     
     if (!menu_navigation.is(":animated") && menu_navigation.is(":visible")) {
         menu_navigation.fadeOut(250, function() {
-            $('html, body').animate({
-                scrollTop: $(targetAnchor).offset().top
+            document.querySelector('html, body').animate({
+                scrollTop: document.querySelector(targetAnchor).offset().top
             }, 300);
         });
     }
 });
+
+
+
+// var menu_navigation = $("#popup--navigation");
+
+// $("#popup--button").on("click", function (e) { 
+//     if (!menu_navigation.is(":animated") && !menu_navigation.is(":visible")) {
+//         menu_navigation.fadeIn(500);
+//     }
+// });
+
+// $("#popup--button--close").on("click", function (e) { 
+//     if (!menu_navigation.is(":animated") && menu_navigation.is(":visible")) {
+//         menu_navigation.fadeOut(500, function() {
+//         });
+//     }
+// });
+
+// $(document).on("click", ".popup-anchor-link", function (e) {
+//     e.preventDefault();
+    
+//     var targetAnchor = $(this).attr("href");
+    
+//     if (!menu_navigation.is(":animated") && menu_navigation.is(":visible")) {
+//         menu_navigation.fadeOut(250, function() {
+//             $('html, body').animate({
+//                 scrollTop: $(targetAnchor).offset().top
+//             }, 300);
+//         });
+//     }
+// });
