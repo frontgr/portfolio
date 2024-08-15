3e9a436665d5780e5c65331cb636af2133cb9600
AlekseySteinberg <117911432+AleksSteinberg@users.noreply.github.com>
2024-08-15 21:11:06 +0400

    fix: Fixed slider

--- front/src/scripts/slider.js
+++ front/src/scripts/slider.js
@@ -6,25 +6,13 @@ $(document).ready(function() {
     const gap = parseInt($('.swiper-wrapper').css('gap'));
 
     $('#rightArrow').click(function() {
-        
-        if (window.innerWidth >= 1440 && slides.length <= 2) {
-            slides.first().toggleClass('FirstCard');
-        }
-        else {
-            currentIndex = (currentIndex + 1) % slides.length;
+        currentIndex = (currentIndex + 1) % slides.length;
             slider.animate({ 'margin-left': -(slideWidth + gap) * currentIndex}, 600);
-        }
     });
 
    $('#leftArrow').click(function() {
-       
-       if (window.innerWidth >= 1440 && slides.length <= 2) {
-        slides.first().toggleClass('FirstCard');
-       }
-       else {
         currentIndex = (currentIndex - 1 + slides.length) % slides.length;
         slider.animate({ 'margin-left': -(slideWidth + gap) * currentIndex}, 600);
-       }
    });
 
 });
